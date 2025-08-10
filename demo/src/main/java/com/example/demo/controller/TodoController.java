5e68d1b2c75aa7fd7881b137fdb7cc4d753c627b
chanbii <2039215k@naver.com>
2025-08-10 17:14:39 +0900

    Add: 서비스 레이어 추가

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -7,16 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
 
 @RestController
 @RequestMapping("todo")
 public class TodoController {
-	@GetMapping("/testResponseEntity")
+	
+	@Autowired // 알아서 빈을 찾아서 그 빈을 이 인스턴스 멤버변수에 연결하라
+	private TodoService service;
+	
+	@GetMapping("/test")
 	public ResponseEntity<?> testTodo(){
+		String str = service.testService();
 		List<String> list = new ArrayList<>();
-		list.add("Hello World! I'm Hiro, and you got 200!");
+		list.add(str);
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.ok().body(response);
 	}
